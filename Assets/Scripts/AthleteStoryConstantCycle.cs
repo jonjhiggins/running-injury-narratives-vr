4741db839f2a256bd2987fc1512b32829d868e21
Jon Higgins <jon@jonhiggins.co.uk>
2023-10-19 16:49:18 +0100

    Refine constant cycle animation

--- Assets/Scripts/AthleteStoryConstantCycle.cs
+++ Assets/Scripts/AthleteStoryConstantCycle.cs
@@ -6,29 +6,11 @@ public class AthleteStoryConstantCycle : AthleteStory
 {
     [SerializeField]
     private Animator animator;
-    [SerializeField]
-    private float startRotateTime;
-    [SerializeField]
-    private float stopRotateTime;
-
-    IEnumerator StartRotation()
-    {
-        yield return new WaitForSeconds(startRotateTime);
-        animator.SetBool("Rotate", true);
-        StartCoroutine(StopRotation());
-    }
-
-    IEnumerator StopRotation()
-    {
-        float rotationDuration = stopRotateTime - startRotateTime;
-        yield return new WaitForSeconds(rotationDuration);
-        animator.SetBool("Rotate", false);
-    }
 
     public override void OnSelect()
     {
         PlayAudio();
-        StartCoroutine(StartRotation());
+        animator.enabled = true;
     }
 
 }
