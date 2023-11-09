5321b3837855bc1d0759f31266140fa289609a55
Jon Higgins <jon@jonhiggins.co.uk>
2023-11-09 17:39:23 +0000

    Remove debug logs

--- Assets/Scripts/TutorialPlayTimelineOnPickUp.cs
+++ Assets/Scripts/TutorialPlayTimelineOnPickUp.cs
@@ -15,7 +15,6 @@ public class TutorialPlayTimelineOnPickUp : MonoBehaviour {
 
     public void WhenSelectTutorialObject()
     {
-        Debug.Log($"WhenSelectTutorialObject {allowTimelinePlay}");
         if (!allowTimelinePlay)
         {
             return;
@@ -27,7 +26,6 @@ public class TutorialPlayTimelineOnPickUp : MonoBehaviour {
     public void AlowTimelinePlay()
     {
         allowTimelinePlay = true;
-        Debug.Log($"AlowTimelinePlay {allowTimelinePlay}");
     }
 
 }
