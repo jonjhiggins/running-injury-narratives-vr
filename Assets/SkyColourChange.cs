694682922d62df6052dcdb1bb8cea8cb9186cac4
Jon Higgins <jon@jonhiggins.co.uk>
2023-11-15 12:55:07 +0000

    Fix sky not changing in merry go round

--- Assets/SkyColourChange.cs
+++ Assets/SkyColourChange.cs
@@ -4,37 +4,69 @@ using UnityEngine;
 
 public class SkyColourChange : MonoBehaviour
 {
+    [System.Serializable]
+    public struct SkyBoxColours
+    {
+        public Color topColor;
+        public Color middleColor;
+        public Color bottomColor;
+    }
 
-    public Color startColor = new Color(0.5f, 0, 0, 1);
-    public Color darkColor = new Color(0.5f, 0, 0, 1);
-    public Color lightColor = new Color(0.5f, 0, 0, 1);
+    public SkyBoxColours startColours;
+    public SkyBoxColours darkColours;
+    public SkyBoxColours lightColours;
+    public SkyBoxColours endColours;
 
     private void Start()
     {
-        RenderSettings.skybox.SetColor("_Tint", startColor);
+        RenderSettings.skybox.SetColor("_BottomColor", startColours.bottomColor);
+        RenderSettings.skybox.SetColor("_MiddleColor", startColours.middleColor);
+        RenderSettings.skybox.SetColor("_TopColor", startColours.topColor);
     }
 
     public void FadeToDark()
     {
-        StartCoroutine(LerpFunction(darkColor, 5));
+        FadeToColours(darkColours);
     }
 
     public void FadeToLight()
     {
-        StartCoroutine(LerpFunction(lightColor, 5));
+        FadeToColours(lightColours);
+    }
+
+    public void FadeToEnd()
+    {
+        FadeToColours(endColours);
+
+    }
+    private void FadeToColours(SkyBoxColours endValues)
+    {
+        var currentColours = GetCurrentColours();
+        StartCoroutine(LerpFunction(currentColours, endValues, 5));
+    }
+
+    private SkyBoxColours GetCurrentColours()
+    {
+        var colors = new SkyBoxColours();
+        colors.bottomColor = RenderSettings.skybox.GetColor("_BottomColor");
+        colors.middleColor = RenderSettings.skybox.GetColor("_MiddleColor");
+        colors.topColor = RenderSettings.skybox.GetColor("_TopColor");
+        return colors;
     }
 
 
-    IEnumerator LerpFunction(Color endValue, float duration)
+    IEnumerator LerpFunction(SkyBoxColours startValues, SkyBoxColours endValues, float duration)
     {
         float time = 0;
+
         while (time < duration)
         {
-            RenderSettings.skybox.SetColor("_Tint", Color.Lerp(startColor, endValue, time / duration));
+            RenderSettings.skybox.SetColor("_BottomColor", Color.Lerp(startValues.bottomColor, endValues.bottomColor, time / duration));
+            RenderSettings.skybox.SetColor("_MiddleColor", Color.Lerp(startValues.middleColor, endValues.middleColor, time / duration));
+            RenderSettings.skybox.SetColor("_TopColor", Color.Lerp(startValues.topColor, endValues.topColor, time / duration));
             time += Time.deltaTime;
             yield return null;
         }
-        RenderSettings.skybox.SetColor("_Tint", endValue);
     }
 
 
